1e5c6ae505b5a70922fe2035c2fc72ce3e70b069
Silutra <otherlayer@pm.me>
2023-03-03 21:00:34 +0100

    Start, stop, admit_event default impl.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use nostr_rs_proto::nauthz_grpc::{EventReply, EventRequest};
+use nostr_rs_proto::nauthz_grpc::{EventReply, EventRequest, Decision};
 
 pub trait Plugin {
-    fn start(&self);
+    fn start(&self) {}
     fn name(&self) -> String;
-    fn admit_event(&self, request: &EventRequest) -> EventReply;
-    fn stop(&self);
+    fn admit_event(&self, _: &EventRequest) -> EventReply {
+        return EventReply { decision: Decision::Permit as i32, message: None }
+    }
+    fn stop(&self) {}
 }
